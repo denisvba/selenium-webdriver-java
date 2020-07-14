56abd8e76596fc85c8d5d1aabc9600c6fcdc5641
Denis Vitor Bueno de Almeida <denis.meida@gmail.com>
2020-07-14 02:43:48 -0300

    add @Before and @After

--- webdriverJava/src/test/java/tests/userInformationTest.java
+++ webdriverJava/src/test/java/tests/userInformationTest.java
@@ -1,6 +1,9 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,18 +14,22 @@ import java.util.concurrent.TimeUnit;
 
 public class userInformationTest {
 
+    private WebDriver browser;
+
+    @Before
+    public void setUp(){
+        // Open Browser
+        //System.setProperty("webdriver.chrome.driver","~/drivers/chromedriver83/chromedriver");
+        browser = new ChromeDriver();
+        browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        // Navigate to Takist page
+        browser.get("http://www.juliodelima.com.br/taskit");
+    }
 
 
     @Test
     public void testAddOneAdditionalUserInfo(){
-        // Open Browser
-        //System.setProperty("webdriver.chrome.driver","~/drivers/chromedriver83/chromedriver");
-        WebDriver browser = new ChromeDriver();
-        browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-
-        // Navigate to Takist page
-        browser.get("http://www.juliodelima.com.br/taskit");
 
         // Click on link with text="Sign in"
         browser.findElement(By.linkText("Sign in")).click();
@@ -50,11 +57,12 @@ public class userInformationTest {
 
         // Validation
         assertEquals("Hi, Julio", meElementText);
+    }
 
+    @After
+    public void TearDown(){
         // Close the browser
         browser.quit(); // close all tabs
         // browser.close(); // close only current tab
-
     }
-
 }
