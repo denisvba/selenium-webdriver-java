8c12223741ef5313b75288c1c56f489836292a75
Denis Vitor Bueno de Almeida <denis.meida@gmail.com>
2020-07-14 01:59:48 -0300

    Introduction to Selenium, WebDriver and first test

---
+++ webdriverJava/src/test/java/tests/userInformationTest.java
@@ -0,0 +1,60 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class userInformationTest {
+
+
+
+    @Test
+    public void testAddOneAdditionalUserInfo(){
+        // Open Browser
+        //System.setProperty("webdriver.chrome.driver","~/drivers/chromedriver83/chromedriver");
+        WebDriver browser = new ChromeDriver();
+        browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+
+        // Navigate to Takist page
+        browser.get("http://www.juliodelima.com.br/taskit");
+
+        // Click on link with text="Sign in"
+        browser.findElement(By.linkText("Sign in")).click();
+
+        // Instantiate a WebElement object
+        // WebElement linkSignIn = browser.findElement(By.linkText("Sign in"));
+        // linkSignIn.click();
+
+        // Id form id="signinbox"
+        WebElement formSignInBox = browser.findElement(By.id("signinbox"));
+
+        // Type on field name="login" inside form id="signinbox" the text="julio0001"
+        formSignInBox.findElement(By.name("login")).sendKeys("julio0001");
+
+        // Type on field name="password" inside form id="signinbox" the text="123456"
+        formSignInBox.findElement(By.name("password")).sendKeys("123456");
+
+        // Click on link with text="SIGN IN"
+        browser.findElement(By.linkText("SIGN IN")).click();
+
+        // Validate element class="me" has text "Hi, Julio"
+
+        WebElement me = browser.findElement(By.className("me"));
+        String meElementText = me.getText();
+
+        // Validation
+        assertEquals("Hi, Julio", meElementText);
+
+        // Close the browser
+        browser.quit(); // close all tabs
+        // browser.close(); // close only current tab
+
+    }
+
+}
